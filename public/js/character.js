ff08b5d38020529807f374812b5fc8f33184afa4
SouthBennett <146032836+SouthBennett@users.noreply.github.com>
2025-11-12 22:25:00 -0800

    Added imaged and js

---
+++ public/js/character.js
@@ -0,0 +1,27 @@
+// Placeholder characters (will add apis later)
+const characters = [
+  {
+    id: 1,
+    name: "Monkey D. Luffy",
+    series: "One Piece",
+    type: "anime",
+    imageUrl: "images/luffy.png",
+    hints: [
+      "He's the captain of a pirate crew.",
+      "He wears a straw hat.",
+      "He wants to become the Pirate King."
+    ]
+  },
+  {
+    id: 2,
+    name: "Naruto Uzumaki",
+    series: "Naruto",
+    type: "anime",
+    imageUrl: "images/naruto.jpg",
+    hints: [
+      "He's a ninja from the Hidden Leaf Village.",
+      "He has a fox spirit sealed inside him.",
+      "He dreams of becoming Hokage."
+    ]
+  }
+];
